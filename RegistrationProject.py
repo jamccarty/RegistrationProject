29c3f437cdbadef03b9fb00bcab811abcf69bb84
Jac McCarty <98111043+jamccarty@users.noreply.github.com>
2022-11-06 14:20:35 -0500

    going to merge with new edits to RegistrationProject.py

--- RegistrationProject.py
+++ RegistrationProject.py
@@ -360,13 +360,12 @@ def classSchedule(constraints_filename, students_filename):
     # df.index = [f'room {r[1]}' for r in maxRoomSize]
     return schedule, globalStudentCount, globalStudentCount / ((len(studentPrefLists) - 1) * 4)
 
-file = open("../basic/output.txt", "w")
-print(file)
-file.write("Course\tRoom\tTeacher\tTime\tStudents\n")
+file = open("output.txt", "wb")
+file.write(bytes("Course\tRoom\tTeacher\tTime\tStudents\n", "UTF-8"))
 schedule, globalStudentCount, score = classSchedule("demo_constraints.txt", "demo_studentprefs.txt")
 
 for time in schedule:
     for clss in time:
-        file.write(f"{clss}\n")
+        file.write(bytes(f"{clss}\n","UTF-8"))
 
 file.close()
