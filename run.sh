52ed6546ff5e6f0b26de6c72dae8a39670b88fe8
Jac McCarty <98111043+jamccarty@users.noreply.github.com>
2022-11-06 15:16:45 -0500

    modified comments in sample run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 for i in 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 do
-    python GENERATE_FILE $1 bla bla #make sure it goes to a specific file filename.py
-    RegistrationProject.py filename.py
+    python GENERATE_FILE $1 bla bla #make sure it goes to a specific files
+    RegistrationProject.py constraints_filename.py studentpref_filename.py #will need to modify RegistrationProject.py to take runtime args
 done
