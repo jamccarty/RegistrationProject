6ce3527afcbe479967504f15748375c1d53532cf
Audrey Yang <audrey.butter0930@gmail.com>
2022-11-06 15:06:49 -0500

    Finished Testing for dept conflicts

--- depart_conflicts.py
+++ depart_conflicts.py
@@ -6,7 +6,8 @@ import DataStructures as ds
         classes, list of classes each with a list of students who wish to take that class 
             (whoPrefers[class], where class is a linked list)
     Returns: 
-        ret, a 2D Array, where for each ret[i,j], it gives the percentage of overlap between class i and j
+        ret, a 2D Array, where for each ret[i,j], it gives the percentage of students in class i
+        who want to take class j
 '''
 def depart_conflicts(classes):
     # initialize the returned matrix
@@ -14,15 +15,10 @@ def depart_conflicts(classes):
 
     for i in range(len(classes)):
         classA = classes[i]
-        print(i)
-        print(classA)
         for j in range(len(classes)):
             classB = classes[j]
-            print(classB)
-            itt = classB.head # how convert itt into something
+            itt = classB.head
             while(itt != None):
-                print("itt!")
-                print(itt)
                 if classA.contains(itt.data):
                     ret[i][j] += 1
                 itt = itt.next
@@ -30,7 +26,7 @@ def depart_conflicts(classes):
 
     return ret
 
-# TODO: make the. the test data
+# Testing
 classes = []
 c1 = ds.LinkedList()
 c1.append(1)
@@ -42,15 +38,23 @@ c2 = ds.LinkedList()
 c2.append(2)
 c2.append(3)
 c2.append(4)
+c2.append(1)
 
 c3 = ds.LinkedList()
 c3.append(1)
 c3.append(4)
 c3.append(5)
 
+c4 = ds.LinkedList()
+c4.append(4)
+c4.append(5)
+c4.append(2)
+c4.append(1)
+
 classes.append(c1)
 classes.append(c2)
 classes.append(c3)
+classes.append(c4)
 
 # print(classes)
 
