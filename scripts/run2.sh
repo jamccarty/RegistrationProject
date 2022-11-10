1e3966eb658b383ab853a93fb57bb811477118c8
Audrey Yang <audrey.butter0930@gmail.com>
2022-11-09 21:28:20 -0500

    fixed echo line in run2

--- scripts/run2.sh
+++ scripts/run2.sh
@@ -2,6 +2,6 @@ for i in 0 1 2 3 4 5 6 7 8 9
 do
     c_file="testA/constraints_$i"
     p_file="testA/prefs_$i"
-    $echo i
+    echo $i
     python RP2.py $c_file $p_file #will need to modify RegistrationProject.py to take runtime args
 done
