faa37a368cfd508d6df509ef55538335e449daaa
El Bartsch <jxbreu@rit.edu>
2022-11-09 20:27:13 -0500

    run sh edit

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,5 @@ for i in 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 do
     c_file="constraints_$i"
     p_file="prefs_$i"
-    RegistrationProject.py c_file p_file #will need to modify RegistrationProject.py to take runtime args
+    python RegistrationProject.py c_file p_file #will need to modify RegistrationProject.py to take runtime args
 done
