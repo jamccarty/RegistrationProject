407bff915e2a6a0d77dd264ebf84625b008b1d67
Audrey Yang <audrey.butter0930@gmail.com>
2022-11-09 21:15:35 -0500

    Removed commas so the array works

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-for i in 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+for i in 0 1 2 3 4 5 6 7 8 9
 do
     c_file="baseA/constraints_$i"
     p_file="baseA/prefs_$i"
--- scripts/run2.sh
+++ scripts/run2.sh
@@ -1,4 +1,4 @@
-for i in 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+for i in 0 1 2 3 4 5 6 7 8 9
 do
     c_file="testA/constraints_$i"
     p_file="testA/prefs_$i"
