afd52f5403d0a58f62115ac396ba3c38b5eb0ab0
Audrey Yang <audrey.butter0930@gmail.com>
2022-11-12 18:04:01 -0500

    Added list of timeslots

--- scripts/realConstraints.py
+++ scripts/realConstraints.py
@@ -20,20 +20,14 @@ parses contents of constraints.txt
 PARAMS:
   file location of constraints.txt
 
-RETURNS: numTimeSlots, rooms, classFacts, domains
+RETURNS: numTimeSlots, rooms, numClasses
     numTimeSlots -- number of time slots (integer)
+    #TODO: time slots array
     rooms -- array of Room() objects - 
         Each Room() object in rooms holds the following pieces of information:
-            id - the id of the room, integer
             capacity - the capacity of the room, integer
-            domain - the domain in which the room is located, string
-            accessible - whether or not the room is accessible, boolean
-    classFacts -- array of classFacts() objects indexed by class id (size is number of classes)
-        Each location in classFacts holds three pieces of information:
-            classFacts[class_id].professor - the professor this class can be taught by
-            classFacts[class_id].major - the major this class contributes to
-            classFacts[class_id].domain - the domain this class must be taught in
-    domains - an array of all domains
+            id - the id of the room, string
+    numClasses -- number of classes (integer)
 '''
 def parseConstraints(filename):
     # open file, split by line
@@ -51,18 +45,70 @@ def parseConstraints(filename):
     numTimeSlots = int(x[1]) # these are the time slots
     print(numTimeSlots)
 
-    # TODO: get all of the time slots (as its own list of some kind - woo)
+    times = []
+    count = 0 # timeslot ID
+    # get all of the timeslots
+    for line in lines[1:numTimeSlots + 1]:
+        x = line.split('\t')
+        # print(x[1]) # GOTTA MCHECKING. SPLICE THE MWF OFF AHHHH
+        x_split = x[1].split(" ")
+        # print(f"Segmented: {x_split}")
+        time_spliced = []
+        for i in x_split:
+            if i != "" and not i.isspace():
+                time_spliced.append(i)
+        # get the days of the week and convert it to a list format
+        days = time_spliced[4]
+        days_ls = []
+        if days == "M-F":
+            days_ls.append("M")
+            days_ls.append("T")
+            days_ls.append("W")
+            days_ls.append("TH")
+            days_ls.append("F")
+        else:
+            for i in days:
+                days_ls.append(i)
+        # print(days_ls)
+
+
+        # get the start time
+        start_time = [] 
+        start_time.append(time_spliced[0])
+        start_time.append(time_spliced[1])
+        sTime = " ".join(start_time)
+
+        # get the end time
+        end_time = [] 
+        end_time.append(time_spliced[2])
+        end_time.append(time_spliced[3])
+        eTime = " ".join(end_time)
+
+        # print(f"Start Time: {sTime}\tEnd Time: {eTime}\t Days:{days}")
+
+        time_slot = mech.TimeSlot(count, mech.Time(sTime), mech.Time(eTime), days_ls)
+        times.append(time_slot)
+        # time_span = " ".join(time_spliced[:len(time_spliced)-1]) # get only the time span
+        # print(f"Rejoined: {time_span}")
+        # print(days)
+        # time_conv = mech.Time(time_span)
+        # print(time_conv)
+
+ 
 
     # get number of rooms
     room = lines[1 + numTimeSlots].split('\t')
     numRooms = int(room[1]) #number of rooms total
     rooms = []
+    count = 1 # this is the "ID"
 
     # initialize rooms array -- array of tuples (room size, room id)
     # (this is so that after merge sorting, each size remains paired with correct room id)
     for line in lines[2+numTimeSlots:2 + numTimeSlots + numRooms]:
         x = line.split('\t')
-        rooms.append((int(x[1]), x[0])) # TODO: POTENTIAL ISSUE - THERE ARE MULTIPLE OF THE SAME ROOM SIZES
+        new_room = mech.Room(x[0], count, int(x[1]))
+        rooms.append(new_room)
+        count += 1
     
     # Get Number of Classes
     loc = 2 + numTimeSlots + numRooms + 1
@@ -81,13 +127,17 @@ def parseConstraints(filename):
         classTeachers.append(int(tc[1]))
 
     file.close() #close file
-    return numTimeSlots, rooms, numClasses
+    return numTimeSlots, rooms, numClasses, times
 
 constraints_filename = "f2_constraints.txt"
-numTimeSlots, maxRoomSize, numClasses = parseConstraints(constraints_filename)
+numTimeSlots, maxRoomSize, numClasses, timeSlots = parseConstraints(constraints_filename)
 
 print(f"Number of Time Slots: {numTimeSlots}")
-print(maxRoomSize)
+# print(maxRoomSize.str)
 # for room in maxRoomSize:
-#     print(f"{room[1]}\t{room[0]}")
+#     print(f"{room.name} \t {room.capacity}")
+#     # print(f"{room[1]}\t{room[0]}")
 print(numClasses)
+# print(timeSlots)
+for time in timeSlots:
+    print(f"{time.start_time}\t{time.end_time}\t{time.days_of_week}")
