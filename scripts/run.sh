3057cd18c578a73fed265dd0abab63a8955ae582
El Bartsch <jxbreu@rit.edu>
2022-11-12 18:24:49 -0500

    is valid run sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,8 @@
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    c_file="testF/constraints_$i"
-    p_file="testF/prefs_$i"
+    c_file="testE/constraints_$i"
+    p_file="testE/prefs_$i"
     echo "File" $i
     python RegistrationProject.py $c_file $p_file #will need to modify RegistrationProject.py to take runtime args
+    perl is_valid.pl $c_file $p_file output.txt
 done
