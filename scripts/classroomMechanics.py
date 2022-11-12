ecb1b4b1d8e0af81175a5b23bfb06723afee8c8a
El Bartsch <jxbreu@rit.edu>
2022-11-12 18:18:54 -0500

    rp2 run changes

--- scripts/classroomMechanics.py
+++ scripts/classroomMechanics.py
@@ -184,10 +184,7 @@ class Room:
         self.domain = domain
         self.accessible = accessible
 
-    def __init__(self, name, id, capacity):
-        self.id = id
-        self.capacity = capacity
-        self.name = name
+    
 
     def __gt__(self, other):
         return self.capacity > other.capacity
