1e3dba2edd6f0ab804ef6ec43b7cd7088f0e4f2a
El Bartsch <jxbreu@rit.edu>
2022-11-13 19:37:32 -0500

    time analysis sheet (incomplete)

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    c_file="testE/constraints_$i"
-    p_file="testE/prefs_$i"
+    c_file="ta/ta_cs_400/constraints_$i"
+    p_file="ta/ta_cs_400/prefs_$i"
     echo "File" $i
     python RegistrationProject.py $c_file $p_file #will need to modify RegistrationProject.py to take runtime args
     perl is_valid2.pl $c_file $p_file output.txt
