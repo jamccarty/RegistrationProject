0e405e73218e4a6b73e36a455d68d670c67ad710
El Bartsch <jxbreu@rit.edu>
2022-11-14 08:28:54 -0500

    bash CLI

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,11 @@
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    c_file="ta/ta_cs_400/constraints_$i"
-    p_file="ta/ta_cs_400/prefs_$i"
+    c_file="testA/constraints_$i"
+    p_file="testA/prefs_$i"
+    if [[ "$#" == "1" ]]; then
+        c_file="$1/constraints_$i"
+        p_file="$1/prefs_$i"
+    fi
     echo "File" $i
     python RegistrationProject.py $c_file $p_file #will need to modify RegistrationProject.py to take runtime args
     perl is_valid2.pl $c_file $p_file output.txt
--- scripts/run2.sh
+++ scripts/run2.sh
@@ -2,6 +2,10 @@ for i in 0 1 2 3 4 5 6 7 8 9
 do
     c_file="testA/constraints_$i"
     p_file="testA/prefs_$i"
+    if [[ "$#" == "1" ]]; then
+        c_file="$1/constraints_$i"
+        p_file="$1/prefs_$i"
+    fi
     echo "File:" $i
     python RP2.py $c_file $p_file #will need to modify RegistrationProject.py to take runtime args
     perl is_valid2.pl $c_file $p_file mod_output.txt
