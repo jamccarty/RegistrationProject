2f3094e375ca0b709882dae2967303cdf72b835b
El Bartsch <jxbreu@rit.edu>
2022-11-14 17:32:36 -0500

    more data

--- scripts/make_random_input.pl
+++ scripts/make_random_input.pl
@@ -30,8 +30,8 @@ if ($numclasses < 20) {
 	print "The number of classes must be 20 or higher so that at least one esem is added.\n";
 	exit 1;
 }
-if ($nummajors > $numclasses){
-	print "The number of majors must be less than or equal to the number of courses.\n";
+if ($nummajors > $numclasses * .25){
+	print "There can be no more than 1 major for every 4 courses.\n";
 	exit 1;
 }
 
